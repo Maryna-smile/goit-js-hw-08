515ed3bcb141849507bfa85274b1d6985e1ec0f5
Maryna-smile <maryna.naidych@gmail.com>
2022-11-10 00:52:00 +0200

    капець

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const makeGallery = (event) => {
+
+    const img = galleryItems.map((item) =>
+        `<a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+      </a>`
+    ).join('');
+
+    galleryList.innerHTML = img;
+};
+makeGallery();
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt'});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const FORM_KEY = 'userData';
+formEl.addEventListener('input', throttle(handleInput, 500));
+formEl.addEventListener('submit', handleSubmit);
+
+function handleInput(event) {
+    //    const savedDate = localStorage.getItem(FORM_KEY) ? JSON.parse(localStorage.getItem(FORM_KEY)) : {};
+    let savedDate = JSON.parse(localStorage.getItem(FORM_KEY)) ?? {}; // оператор нульового злиття
+
+    const { name, value } = event.target;
+
+    savedDate = {
+        ...savedDate,
+        [name]: value,
+    };
+
+    localStorage.setItem(FORM_KEY, JSON.stringify(savedDate));
+}
+
+const renderData = () => {
+    let dataToRender = JSON.parse(localStorage.getItem(FORM_KEY)) ?? {}; // оператор нульового злиття
+
+    const inputNames = Object.keys(dataToRender);
+
+    inputNames.forEach(inputName => {
+        let input = formEl.elements[inputName];
+        let valueKey = 'value';
+        input[valueKey] = dataToRender[inputName];
+    });
+};
+renderData();
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    let finalData = {};
+    const formData = new FormData(formEl);
+    for (const [name, value] of formData.entries()) {
+
+        finalData[name] = value;
+    }
+
+    localStorage.removeItem(FORM_KEY);
+    console.log(finalData);
+    formEl.reset();
+}
